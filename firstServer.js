99eeb70348d61b897f161692f77b8d3f0c95d93e
CrappyDill <mddillon47@gmail.com>
2021-10-14 16:47:30 -0700

    use() and next()

--- firstServer.js
+++ firstServer.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 
 
+app.use((req, res, next) => {
+    console.log("Server got a request!")
+    next();
+})
 
-
+app.use((req, res, next) => {
+    console.log("Server got another request!")
+    next();
+}) 
 
 app.listen(3000, () => {
     console.log('App on port 3000');
